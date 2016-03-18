2c69f0fe59ac6ab8153ec9adf5c1c7c245990e00
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-03-17 22:52:46 -0400

    style list

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 var $ul = $('ul');
-var $li = $('<li>');
 
-grains.forEach (function () {
-  $ul.append ($(this));
+grains.forEach(function (glob) {
+
+// Why do the variables need to go in this loop for it to work?
+
+  var $li = $('<li>');
+  var $img = $('<img>');
+  var $p = $('<p>');
+  var $h2 = $('<h2>');
+
+  $ul.append($li);
+  $li.append($img, $h2, $p);
+
+  $img.attr('src', 'images/' + glob.img);
+  $h2.html(glob.name);
+  $p.html(glob.desc);
+
 });
